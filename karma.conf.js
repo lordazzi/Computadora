dbb1b074c98e15a12b416c0ea98db86bbfb2866e
lordazzi <ricardoazzi91@hotmail.com>
2017-09-22 20:47:50 -0300

    Garantindo execução dos testes unitários

--- karma.conf.js
+++ karma.conf.js
@@ -4,19 +4,21 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: [
-        'jasmine'
+      'jasmine'
     ],
     plugins: [
+      require("karma-webpack"),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter')
     ],
-    client:{
+    mime: { 'text/x-typescript': ['ts','tsx'] },
+    client: {
       clearContext: true // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     customLaunchers: {
@@ -30,8 +32,46 @@ module.exports = function (config) {
       }
     },
     files: [
-        'src/*.spec.ts'
+      {
+        pattern: 'src/**/*.spec.ts'
+      }
     ],
+    preprocessors: {
+      'src/**/*.ts': ['webpack']
+    },
+    webpack: {
+      resolve: {
+        extensions: ['.ts', '.js'],
+        modules: ['node_modules']
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            use: [
+              {
+                loader: 'awesome-typescript-loader',
+                query: {
+                  sourceMap: false,
+                  inlineSourceMap: true,
+                  compilerOptions: {
+                    removeComments: true
+                  }
+                }
+              }
+            ]
+          }
+        ]
+      },
+      node: {
+        global: true,
+        process: false,
+        crypto: 'empty',
+        module: false,
+        clearImmediate: false,
+        setImmediate: false
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: "./src/main.ts",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [
+            '.ts'
+        ]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.ts$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    }
+};
