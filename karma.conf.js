cb5f86e3ff60438559ef44fe611490127cb71323
lordazzi <ricardoazzi91@hotmail.com>
2017-09-22 01:35:08 -0300

    Revisando bibliotecas usadas para escrever os testes

--- karma.conf.js
+++ karma.conf.js
@@ -1,59 +1,43 @@
+// continuar depois a partir deste tutorial
+// https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
 module.exports = function (config) {
-    config.set({
-
-        basePath: '',
-
-        plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-typescript-preprocessor'),
-            require('karma-systemjs')
+  config.set({
+    basePath: '',
+    frameworks: [
+        'jasmine'
+    ],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter')
+    ],
+    client:{
+      clearContext: true // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
+    },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222',
         ],
-
-        frameworks: [
-            'systemjs',
-            'jasmine'
-        ],
-
-        files: [
-            'src/*.spec.ts'
-        ],
-
-        preprocessors: {
-            '*.ts': ['typescript']
-        },
-
-        systemjs: {
-            configFile: 'systemjs.config.js',
-            serveFiles: [
-                'src/**/*.ts'
-            ]
-        },
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
-
-        // web server port
-        port: 9876,
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: [ 'Chrome' ],
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
-    });
+      }
+    },
+    files: [
+        'src/*.spec.ts'
+    ],
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false
+  });
 };
